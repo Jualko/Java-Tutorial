71a0088d4b417cb3aecc38c173f609fe9d26e714
Jualko <66382948+Jualko@users.noreply.github.com>
2020-07-07 23:39:05 +0200

    inital commit
    basics: primitive data types

---
+++ Tutorial1/src/Application.java
@@ -0,0 +1,42 @@
+/**
+ * @author Justin
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+		
+		// primitive data types
+
+		// 32-bit
+		int myNumber;
+		myNumber = 20200707;
+		// 16-bit
+		short myShort = 27;
+		// 64-bit
+		long myLong = 28;
+
+		// double-precision 64-bit
+		double myDouble = 3.14;
+		// single-precision 32-bit
+		float myFloat = 3.14f;
+
+		// 8-bit
+		byte myByte = 127;
+
+		// single 16-bit character
+		char myChar = 'j';
+
+		boolean myBool = true;
+
+		System.out.println(myNumber);
+
+	}
+
+}
