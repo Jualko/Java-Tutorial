35572e599b926f6491bca2e79616fddb4d8372e7
Jualko <66382948+Jualko@users.noreply.github.com>
2020-07-09 23:57:33 +0200

    loops

---
+++ Tutorial3 - loops/src/Application.java
@@ -0,0 +1,20 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		// while loop
+		int value = 0;
+
+		while (value < 10) {
+			System.out.println("While Loop " + value);
+			value++;
+		}
+
+		// for loop
+		for (int i = 0; i < 5; i++) {
+			System.out.printf("For Loop %d\n", i);
+		}
+	}
+
+}
