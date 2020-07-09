8df7a6293d0cc36ec414b283fac1ac55c3582798
Jualko <66382948+Jualko@users.noreply.github.com>
2020-07-09 23:36:08 +0200

    string: working with text

---
+++ Tutorial2/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//String: class
+		//"Hello World": instance of that class (object)
+		String text = "Hello";
+		String name = "World";
+		String greeting = text + " " + name;
+		
+		System.out.println(greeting);
+		
+	}
+
+}
