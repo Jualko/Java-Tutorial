c5469c60f980a8f9c9edf9be3345ddb94a994ffd
Jualko <66382948+Jualko@users.noreply.github.com>
2020-07-16 23:50:58 +0200

    2d arrays

--- Tutorial7 - Multi-Dimensional Arrays/src/App.java
+++ Tutorial7 - Multi-Dimensional Arrays/src/App.java
@@ -2,8 +2,30 @@
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// Multi-Dimensional Array: Array of Array
+		int[][] grid = {
+				{ 11, 12, 13 },
+				{ 21, 22, 23 },
+				{ 31, 32, 33 }
+				};
+
+		System.out.println(grid[1][2]);
+		
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				// \t - tab character
+				System.out.print(grid[i][j] + "\t");
+			}
+			// new line after each row
+			System.out.println();
+		}
+
+		// Array of an Array of Strings
+		// (technically a 2D Array of references to Strings)
+		String[][] texts = new String[2][3];
+
+		System.out.println(texts[0][2]);
 	}
 
 }
