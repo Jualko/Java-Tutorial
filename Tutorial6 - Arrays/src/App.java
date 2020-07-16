6b7c2a760b80219a1ba60deee57fb04047069235
Jualko <66382948+Jualko@users.noreply.github.com>
2020-07-16 23:40:44 +0200

    array of strings

--- Tutorial6 - Arrays/src/App.java
+++ Tutorial6 - Arrays/src/App.java
@@ -3,11 +3,14 @@ public class App {
 
 	public static void main(String[] args) {
 
+		// enough memory to hold an int
+		int value = 123;
+
 		// 'values' is a reference - no memory is allocated yet
 		int[] values;
 
 		// 'values' points to memory being able to hold 3 integers
-		// all values in an arrays have a default value
+		// all values in an arrays have a default value (int: 0)
 		values = new int[3];
 
 		// specifying a value
@@ -22,9 +25,24 @@ public class App {
 		
 		// initialize and declare an array at the same time
 		int[] numbers = { 5, 6, 7 };
+
 		for (int i = 0; i < numbers.length; i++) {
 			int j = numbers[i];
 			System.out.println(j);
+		}
+
+		System.out.println("---");
+
+		// array of strings
+		// default values for Strings: null
+		String[] words = new String[2];
+
+		// this actually allocates the memory for a String
+		words[0] = "Hello";
+		words[1] = "World";
+
+		for (String word : words) {
+			System.out.println(word);
 		}
 	}
 }
